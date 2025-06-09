bc9106535b139c18c7c6f4cf05467dd83b9d477d
Codehub169 <codehub@169pi.com>
2025-06-09 17:14:02 +0530

    Applied user request: I don't see any output in browser there is white screen please check the error and fix it

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { CartProvider } from './contexts/CartContext';
-import { AuthProvider } from './contexts/AuthContext'; // Added
-import customTheme from './theme';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux"; // Added import for Redux Provider
+import store from "./store/store.js"; // Added import for Redux store (assuming path client/src/store/store.js)
+import SearchContextProvider from "./context/SearchContext.jsx";
+import CartContextProvider from "./context/CartContext.jsx";
+import WishlistContextProvider from "./context/WishlistContext.jsx";
+import { theme } from "./utils/theme.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider theme={customTheme}>
-        <AuthProvider> {/* AuthProvider wraps CartProvider and App */}
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </AuthProvider>
+    <Provider store={store}> {/* Wrapped application with Redux Provider */}
+      <ChakraProvider theme={theme}>
+        <SearchContextProvider>
+          <CartContextProvider>
+            <WishlistContextProvider>
+              <BrowserRouter>
+                <App />
+              </BrowserRouter>
+            </WishlistContextProvider>
+          </CartContextProvider>
+        </SearchContextProvider>
       </ChakraProvider>
-    </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
